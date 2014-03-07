05e3d9ed21b3271dd706bdacc64ff80fece3d235
Yoshiyuki Kanno <nekotaroh@gmail.com>
2014-03-07 16:40:42 +0900

    Notify add/delete bucket to gateways

--- src/leo_gateway_api.erl
+++ src/leo_gateway_api.erl
@@ -42,6 +42,8 @@
 -export([set_endpoint/1, delete_endpoint/1,
          update_acl/3]).
 
+-export([add_bucket/3, delete_bucket/3]).
+
 
 %%----------------------------------------------------------------------
 %% API-Function(s)
@@ -211,3 +213,17 @@ update_acl(?CANNED_ACL_AUTHENTICATED_READ, AccessKey, Bucket) ->
     leo_s3_bucket:update_acls2authenticated_read(AccessKey, Bucket);
 update_acl(_,_,_) ->
     {error, invalid_args}.
+
+%% @doc Add a bucket(S3-API)
+%%
+-spec(add_bucket(binary(), binary(), atom()) ->
+             ok | {error, any()}).
+add_bucket(AccessKey, Bucket, _Atom) ->
+    leo_s3_bucket:put(AccessKey, Bucket, _Atom).
+
+%% @doc Delete a bucket(S3-API)
+%%
+-spec(delete_bucket(binary(), binary(), atom()) ->
+             ok | {error, any()}).
+delete_bucket(AccessKey, Bucket, _Atom) ->
+    leo_s3_bucket:delete(AccessKey, Bucket, _Atom).
