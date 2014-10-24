c43c3e2edbfb727558180d091b69ff46a85af4fb
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-24 13:17:53 +0900

    Updated for leo_watchdog

--- src/leo_gateway_sup.erl
+++ src/leo_gateway_sup.erl
@@ -49,11 +49,11 @@ init([]) ->
     WD_MaxMemCapacity = ?env_watchdog_max_mem_capacity(),
     WD_CheckInterval  = ?env_watchdog_check_interval(),
 
-    WatchDog = {leo_gateway_watchdog,
-                {leo_gateway_watchdog, start_link,
-                 [ WD_MaxMemCapacity, WD_CheckInterval ]},
-                permanent,
-                ?SHUTDOWN_WAITING_TIME,
-                worker,
-                [leo_gateway_watchdog]},
-    {ok, {_SupFlags = {one_for_one, 5, 60}, [WatchDog]}}.
+    WatchDogs = [{leo_watchdog_rex,
+                  {leo_watchdog_rex, start_link,
+                   [WD_MaxMemCapacity, WD_CheckInterval]},
+                  permanent,
+                  ?SHUTDOWN_WAITING_TIME,
+                  worker,
+                  [leo_watchdog_rex]}],
+    {ok, {_SupFlags = {one_for_one, 5, 60}, WatchDogs}}.
