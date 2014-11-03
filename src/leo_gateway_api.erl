6f8a220c8c2c81e05781bb943ea5708b3fc93f13
Yosuke Hara <yousuke.hara@gmail.com>
2014-11-03 14:21:13 +0900

    Updated for latest leo_watchdog

--- src/leo_gateway_api.erl
+++ src/leo_gateway_api.erl
@@ -119,8 +119,8 @@ get_node_status() ->
             {rex_max_mem_capacity, ?env_watchdog_max_mem_capacity(leo_gateway)},
             {cpu_max_cpu_load_avg, ?env_watchdog_max_cpu_load_avg(leo_gateway)},
             {cpu_max_cpu_util,     ?env_watchdog_max_cpu_util(leo_gateway)},
-            {io_max_input_for_interval,  ?env_watchdog_max_input_per_sec(leo_gateway)},
-            {io_max_output_for_interval, ?env_watchdog_max_output_per_sec(leo_gateway)}
+            {io_max_input_per_sec,  ?env_watchdog_max_input_per_sec(leo_gateway)},
+            {io_max_output_per_sec, ?env_watchdog_max_output_per_sec(leo_gateway)}
            ]
           },
           {statistics,    Statistics},
