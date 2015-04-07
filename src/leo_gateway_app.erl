ae80875880203f1922883dd8eaabe388c011f977
Yosuke Hara <yousuke.hara@gmail.com>
2015-04-07 18:08:23 +0900

    Fix leo-project/leofs/issues/328 and leo-project/leofs/issues/340

--- src/leo_gateway_app.erl
+++ src/leo_gateway_app.erl
@@ -122,6 +122,20 @@ prep_stop(_State) ->
     catch leo_redundant_manager_sup:stop(),
     catch leo_mq_sup:stop(),
     catch leo_logger_sup:stop(),
+
+    case get_options() of
+        {ok, HttpOptions} ->
+            case HttpOptions#http_options.handler of
+                ?PROTO_HANDLER_S3 ->
+                    leo_gateway_s3_api:stop();
+                ?PROTO_HANDLER_REST ->
+                    leo_gateway_rest_api:stop();
+                _ ->
+                    void
+            end;
+        _ ->
+            void
+    end,
     ok.
 
 %% @spec stop(_State) -> ServerRet
