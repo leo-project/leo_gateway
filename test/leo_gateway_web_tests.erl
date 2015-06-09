789478f870a761f300c272d395ef2bab87b71ba4
Wilson Li <windkithk@gmail.com>
2015-06-09 12:33:44 +0900

    Updates Unit Test

--- test/leo_gateway_web_tests.erl
+++ test/leo_gateway_web_tests.erl
@@ -660,7 +660,7 @@ range_object_base([_TermFun, _Node0, Node1], RangeValue) ->
                                                  ?TARGET_HOST,
                                                  ":8080/a/b.png"]),
                                    [{"connection", "close"},{"range", RangeValue}]}, [], []),
-                ?assertEqual(206, SC),
+                ?assertEqual(200, SC),
                 ?assertEqual("od", Body)
             catch
                 throw:Reason ->
